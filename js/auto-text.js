7a0036f7b0062da5e7fd1a043d7344e505c18cda
chrishardy100 <chrishardy100@gmail.com>
2023-02-24 15:23:48 -0500

    v1.3
    Title for main page.

---
+++ js/auto-text.js
@@ -0,0 +1,23 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'Welcome to our store!'
+let idx = 1
+let speed = 400
+
+writeText()
+
+function writeText(){
+  textEl.innerText = text.slice(0, idx)
+
+  idx++
+
+  if(idx>text.length){
+    idx=1
+  }
+
+  setTimeout(writeText, speed)
+
+
+}
+
+speedEl.addEventListener('input', (e) => speed = 100/e.target.value)
--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-
-const toggle = document.getElementById('toggle');
-const nav = document.getElementById('nav');
-
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-})
-
-
 // alert("MESSAGE");
 // document.getElementById("button").onclick = function(){
 //   document.getElementById("confirm").innerHTML = "Submitted!";
 // }
+
+
+
+
